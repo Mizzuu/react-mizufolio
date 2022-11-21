44fba591df6d66105707dc3543bf1e04c3a4e5bb
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2022-11-20 19:28:55 -0800

    add header section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="home">
+            <div className="header">
+                <h1>Mizuho Tohma</h1>
+                <h2>Front-end Developer</h2>
+                <p>
+                    When I finished one of my team projects, our instructor told
+                    us that our app would be used by kids for an education
+                    purpose. This really touched me because I realized tech can
+                    help people dynamically. Once you crate an app, it could
+                    benefit anyone in this world.
+                </p>
+                <p>
+                    I have a three year flight attendant experience which is 180
+                    degrees different from a developer. Thanks to the very
+                    unique environment that I was working with different
+                    colleagues and passengers every day, I earned great
+                    communication skills to build a good teamwork. I am very
+                    sure I utilize this skills as a developer.
+                </p>
+            </div>
+        </div>
+    );
+};
 
 export default App;
